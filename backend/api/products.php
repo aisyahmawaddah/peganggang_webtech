75c30250199ae7667aa0a1cf17bae15b6df17087
NURFARAH IZZATI <nurfarah.izzati@graduate.utm.my>
2025-06-15 01:54:56 +0800

    api folder and database folder

---
+++ backend/api/products.php
@@ -0,0 +1,5 @@
+<?php
+include_once '../config/cors.php';
+include_once '../config/database.php';
+// ... all the API endpoint logic for products
+?>
---
+++ backend/api/updates.php
@@ -0,0 +1,5 @@
+<?php
+include_once '../config/cors.php';
+include_once '../config/database.php';
+// ... API endpoint logic for updates
+?>
