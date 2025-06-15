3b3cfb452051d4f815f7692b9dfb0b61f72685da
NURFARAH IZZATI <nurfarah.izzati@graduate.utm.my>
2025-06-15 17:12:51 +0800

    aku taktau aku ubah apa banyak sangat

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
-// Configure axios defaults
+// Configure axios defaults - CORRECT URL for your setup
 axios.defaults.baseURL = 'http://localhost/backend/api'
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 axios.defaults.headers.common['Accept'] = 'application/json'
@@ -14,7 +14,11 @@ axios.defaults.withCredentials = false
 // Add request interceptor for debugging
 axios.interceptors.request.use(
   config => {
-    console.log('Making request to:', config.baseURL + config.url);
+    // Automatically add .php extension if not present
+    if (config.url && !config.url.includes('.php') && !config.url.includes('?')) {
+      config.url = config.url + '.php';
+    }
+    console.log('Making request to:', config.baseURL + '/' + config.url);
     console.log('Request config:', config);
     return config;
   },
